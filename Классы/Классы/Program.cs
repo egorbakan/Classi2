a4e5101c2d46ff52698fd14af4cd9bd3dc8ec0c6
egorbakan <118420911+egorbakan@users.noreply.github.com>
2022-11-18 15:22:19 +0900

    Add files via upload

---
+++ Классы/Классы/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Классы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Users user = new Users()
+            {
+                Name = "Иван",
+                Surname = "Иванов",
+                Login = "123",
+                Password = "123",
+            };
+            Console.WriteLine(user.Name);
+            Console.WriteLine("Введите имя");
+            user.Name = Console.ReadLine();
+            Console.WriteLine($"Ваше новое имя - {user.Name}");
+            Console.ReadLine();
+        }
+
+        class Users
+        {
+            public string Name;
+            public string Surname;
+
+            public string Login;
+            public string Password;
+        }
+    }
+}
---
+++ Классы/Классы/User2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Классы
+{
+    class User2
+    {
+        public string Name;
+        public string Surname;
+
+        public string Login;
+        public string Password;
+    }
+}
